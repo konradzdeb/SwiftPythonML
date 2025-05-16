6f2ca23033e6597f956f7b73b0d0f6a0c849b021
Konrad Zdeb <konrad.zdeb@me.com>
2025-05-16 14:21:53 +0100

    Initial commit
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

---
+++ MyMLApp/MyMLApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMLApp
+//
+//  Created by Konrad on 16/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
