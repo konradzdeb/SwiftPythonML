0cecdf9bed544eabf68d2be3a4e76cda60164ec5
Konrad Zdeb <konrad.zdeb@me.com>
2025-05-16 14:21:53 +0100

    Initial commit
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

---
+++ MyMLApp/MyMLApp/MyMLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMLAppApp.swift
+//  MyMLApp
+//
+//  Created by Konrad on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyMLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMLApp/MyMLAppTests/MyMLAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMLAppTests.swift
+//  MyMLAppTests
+//
+//  Created by Konrad on 16/05/2025.
+//
+
+import Testing
+@testable import MyMLApp
+
+struct MyMLAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
